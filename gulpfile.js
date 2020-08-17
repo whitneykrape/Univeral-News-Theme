0a923b78bb18ebefa98a94ca42ac7bd0c0e07cc3
formfunctionio <whitney@formfunction.io>
2020-08-16 23:12:36 -0400

    Committing Gulp, but should be merged...

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,13 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+var del = require('del');
 var runSequence = require('gulp4-run-sequence');
+
+// Development Tasks 
+// -----------------
 
 // Start browserSync server
 gulp.task('browserSync', function() {
@@ -19,10 +24,10 @@ gulp.task('browserSync', function() {
 
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss/**/*.scss') 
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css')) 
-    .pipe(browserSync.reload({ 
+  return gulp.src('assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(gulp.dest('assets/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
 })
@@ -34,6 +39,9 @@ gulp.task('watch', function() {
   // gulp.watch('app/js/**/*.js', browserSync.reload);
 })
 
+// Optimization Tasks 
+// ------------------
+
 // Optimizing CSS and JavaScript 
 gulp.task('useref', function() {
 
@@ -44,6 +52,37 @@ gulp.task('useref', function() {
     .pipe(gulp.dest('dist'));
 });
 
+// Optimizing Images 
+/*
+gulp.task('images', function() {
+  return gulp.src('assets/images/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// Copying fonts 
+gulp.task('fonts', function() {
+  return gulp.src('assets/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Cleaning 
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/*', '!dist/images', '!dist/images/*']);
+});
+*/
+
+// Build Sequences
+// ---------------
 
 gulp.task('default', function(callback) {
   runSequence(['sass', 'browserSync'], 'watch',
@@ -55,6 +94,7 @@ gulp.task('build', function(callback) {
   runSequence(
     'clean:dist',
     'sass',
+    // ['useref', 'images', 'fonts'],
     ['useref'],
     callback
   )
