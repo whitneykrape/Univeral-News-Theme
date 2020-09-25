c8e86d31fd4f4020b048568d45a922dc48e36ea2
Whitney Krape <whitney@formfunction.io>
2020-09-24 23:34:01 -0400

    Updating the Netlify previews. Just a start.

--- base-netlify-hugo/src/js/cms-preview-templates/components/jumbotron.js
+++ base-netlify-hugo/src/js/cms-preview-templates/components/jumbotron.js
@@ -4,18 +4,18 @@ export default class Jumbotron extends React.Component {
   render() {
     const {image, title, subtitle} = this.props;
     return <div>
-      <div className="pv5 pv6-l ph3 bg-center cover" style={{
+      <div className=" " style={{
         backgroundImage: image && `url(${image})`
       }}>
-        <div className="mw7 center ph3">
-          <div className="db mb3">
-            <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+        <div className=" ">
+          <div className=" ">
+            <div className=" ">
+              <h1 className=" ">
                 { title }
               </h1>
             </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
+            <div className=" ">
+              {subtitle && <p className=" ">{ subtitle }</p>}
             </div>
           </div>
         </div>
