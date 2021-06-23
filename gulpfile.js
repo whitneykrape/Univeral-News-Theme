0b11188070153bde83167b6aabe1e68281b058e3
Whitney Krape <whitney@formfunction.io>
2021-06-22 23:45:21 -0400

    Moving more assets into place. Rebuilding the gulp watch. Fixing some quirks of Normalize... Likely will need to revisit some of those.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const postcssNormalize = require('postcss-normalize');
 var cssimport = require("gulp-cssimport");
+var gulpAutoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
@@ -64,16 +66,14 @@ gulp.task('php', () => {
 
 gulp.task('sass', function() {
   return gulp.src('src/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(cssimport({
-      matchPattern: "*.css"
-    }))
+    .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed',
       compass: true,
-      bundleExec: true,
-      sourcemap: true,
-      sourcemapPath: './'
-    }).on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+      bundleExec: true    
+    }))
+    .pipe(gulpAutoprefixer())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist/assets/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
@@ -82,8 +82,11 @@ gulp.task('sass', function() {
 
 // Watchers
 gulp.task('watch', function() {
-  gulp.watch('assets/scss/**/*.scss', { usePolling: true }, gulp.series('sass'));
-  gulp.watch('base-html/*.html', browserSync.reload);
+  gulp.watch('src/assets/scss/**/*.scss', { usePolling: true }, gulp.series('sass'));
+  gulp.watch('./src/partials/blocks/*.hbs', { usePolling: true }, gulp.series('html'));
+  gulp.watch('./src/partials/includes/*.hbs', { usePolling: true }, gulp.series('html'));
+  gulp.watch('./src/partials/layouts/*.hbs', { usePolling: true }, gulp.series('html'));
+
   // gulp.watch('app/js/**/*.js', browserSync.reload);
 })
 
