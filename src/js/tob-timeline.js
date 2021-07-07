c7eb67b00209b2fb7a3722cbc0814d7d57b737ee
Whitney Krape <whitney@formfunction.io>
2021-07-07 07:24:31 -0400

    Removed the need for different homepages by using vars. Roughing in the starts of each block structure and layout. Content update.

---
+++ src/js/tob-timeline.js
@@ -0,0 +1,15 @@
+	// Each! 
+	$eventsDebug = {}
+
+	$timeline = document.getElementById("tob-timeline_number")
+
+	$timelineStage = $timeline.querySelector('.tobt__stage')
+	$timelineEvents = $timeline.querySelector('.tobt__events')
+	$timelineEventFirst = $timelineEvents.firstElementChild
+
+	$timelineStage.querySelector('.tobt__slide').innerHTML = $timelineEvents.firstElementChild.innerHTML
+
+	$eventsDebug.firstEvent = $timelineEventFirst
+
+	console.log('EventsDebug')
+	console.log($eventsDebug)
