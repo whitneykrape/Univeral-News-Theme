d29824f23f0edcdd89ecbbc6095492294275f5cf
Whitney Krape <whitney@formfunction.io>
2022-03-28 20:34:23 -0400

    Starting more blocks...

---
+++ dist/html/blocks/block-feature.js
@@ -0,0 +1,36 @@
+/* wordpress-start */
+<article { ...blockProps }>
+    <a
+        className="wp-block-image article-image imagelink"
+        href={ imagelink }
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        { mediaURL && <img src={ mediaURL } alt="" /> }
+    </a>
+    <div className="article-text-block">r
+        <RichText.Content
+            tagName="h3"
+            className="article-title"
+            value={ title }
+        />
+        <RichText.Content
+            tagName="span"
+            className="image-marker image-marker-date"
+            value={ date }
+        />
+        <RichText.Content
+            tagName="p"
+            className="article-description"
+            value={ description }
+        />
+        <RichText.Content tagName="p" className="article-read-more">
+            <RichText.Content
+                tagName="span"
+                className="btn-sm article-link"
+                value={ link }
+            />
+        </RichText.Content>
+    </div>
+</article>
+/* wordpress-end */
