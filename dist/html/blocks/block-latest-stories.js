6e8957ae6425a6ccc18cfa8521a1cf9279f303b4
Whitney Krape <whitney@formfunction.io>
2022-04-07 20:38:29 -0400

    Adding more blocks.

---
+++ dist/html/blocks/block-latest-stories.js
@@ -0,0 +1,18 @@
+
+/* wordpress-start */
+<div { ...blockProps }>
+	<div class="container">
+		<RichText.Content
+            tagName="h1"
+            className="title"
+            value={ title }
+        />
+
+        <RichText.Content
+            tagName="p"
+            className="subtitle"
+            value={ subtitle }
+        />
+	</div>
+</div>
+/* wordpress-end */
