e1e6c2cc972adcaf9753229e1bf6e0f6f35efb8e
Whitney Krape <whitney@formfunction.io>
2022-05-25 21:11:08 -0400

    Otput...

---
+++ base-wordpress/wordpress/wp-content/themes/twentytwentytwo-slimmed/inc/block-article.js
@@ -0,0 +1,19 @@
+/* wordpress-start */
+/* Change to a block made in WordPress that can be loaded as a pattern. */
+<!-- wp:columns -->
+<div class="wp-block-columns"
+    ><!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis:50%">
+        <!-- wp:post-featured-image /-->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis:50%">
+        <!-- wp:post-title /-->
+
+        <!-- wp:post-excerpt /-->
+    </div>
+    <!-- /wp:column --></div>
+<!-- /wp:columns -->
+/* wordpress-end */
