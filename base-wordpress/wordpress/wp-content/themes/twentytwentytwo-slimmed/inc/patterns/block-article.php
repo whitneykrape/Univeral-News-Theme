7e1ad6207a8de8440ede18a3c471fe48b382d5ab
Whitney Krape <whitney@formfunction.io>
2022-06-15 06:36:10 -0400

    WrdPrss.

--- base-wordpress/wordpress/wp-content/themes/twentytwentytwo-slimmed/inc/patterns/block-article.php
+++ base-wordpress/wordpress/wp-content/themes/twentytwentytwo-slimmed/inc/patterns/block-article.php
@@ -1,26 +1,27 @@
+<?php
 /* wordpress-start */
 /* Change to a block made in WordPress that can be loaded as a pattern. */
-<?php
 /**
  * Article Block
  */
 return array(
-    'title'      => __( 'Article Block', 'single-2022' ),
+    'title'      => __( 'Article Block', single-2022' ),
     'categories' => array( 'single' ),
-    'content'    => '<!-- wp:columns --><div class="wp-block-columns"
-    ><!-- wp:column {"width":"50%"} -->
-    <div class="wp-block-column" style="flex-basis:50%">
-        <!-- wp:post-featured-image /-->
-    </div>
-    <!-- /wp:column -->
+    'content'    => '<!-- wp:columns -->
+        <div class="wp-block-columns">
+        <!-- wp:column {"width":"50%"} -->
+        <div class="wp-block-column" style="flex-basis:50%">
+            <!-- wp:post-featured-image /-->
+        </div>
+        <!-- /wp:column -->
 
-    <!-- wp:column {"width":"50%"} -->
-    <div class="wp-block-column" style="flex-basis:50%">
-        <!-- wp:post-title /-->
+        <!-- wp:column {"width":"50%"} -->
+        <div class="wp-block-column" style="flex-basis:50%">
+            <!-- wp:post-title /-->
 
-        <!-- wp:post-excerpt /-->
-    </div>
-    <!-- /wp:column --></div>
-<!-- /wp:columns -->,
+            <!-- wp:post-excerpt /-->
+        </div>
+        <!-- /wp:column --></div>
+    <!-- /wp:columns -->',
 );
 /* wordpress-end */
