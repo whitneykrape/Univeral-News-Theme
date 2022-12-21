4d03a13bb9fde6ccef55a741316625828df0c419
Whitney Krape <whitney@formfunction.io>
2022-12-20 21:54:32 -0500

    WordPress update.

--- base-wordpress/wordpress/wp-content/plugins/block-article/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b863bd2b9388b4aa3f919c74de5a2434');
