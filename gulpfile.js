2c3c7a0909635f35ceaca2260d1f32bcf98a1dcf
Whitney Krape <whitney@formfunction.io>
2023-03-05 20:56:32 -0500

    Netlify structure.

--- gulpfile.js
+++ gulpfile.js
@@ -194,6 +194,18 @@ gulp.task('blocks-hyas', () => {
       batch: ['./src/blocks'],
       handlebars_helpers
     })) */
+    .pipe(stripCode({
+      start_comment:  "html-start",
+      end_comment:    "html-end"
+    }))
+    .pipe(stripCode({
+      start_comment:  "wordpress-start",
+      end_comment:    "wordpress-end"
+    }))
+    .pipe(stripCode({
+      start_comment:  "craft-start",
+      end_comment:    "craft-end"
+    }))
     .pipe(rename({
       extname: '.html'
     }))
