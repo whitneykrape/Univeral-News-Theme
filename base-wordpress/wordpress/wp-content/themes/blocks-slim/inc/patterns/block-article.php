08019f64bb5a5f1da6a29a667d3bbaf4d2637b85
Whitney Krape <whitney@formfunction.io>
2023-03-22 10:44:59 -0400

    Redoing the article preview, starting.

--- base-wordpress/wordpress/wp-content/themes/blocks-slim/inc/patterns/block-article.php
+++ base-wordpress/wordpress/wp-content/themes/blocks-slim/inc/patterns/block-article.php
@@ -2,14 +2,23 @@
 return array(
     'title'      => __( 'Block Article', 'blocksslim' ),
     'categories' => array( 'slim' ),
-    'content'    => '<!-- wp:block-article/article-block {"mediaID":21,"imagelink":"http://ff-pitin.local/_becauseyouhaveto"} -->
-        <article class="wp-block-block-article-article-block">
-            <a class="wp-block-image article-image imagelink" href="http://ff-pitin.local/_becauseyouhaveto" target="_blank" rel="noopener noreferrer"><img src="http://FF-PITIN.local/_becauseyouhaveto/sparewordpress/wp-content/uploads/2022/01/placeholder.jpg" alt=""/></a>
-            <div class="article-text-block">
-                <h3 class="article-title">test</h3>
-                <span class="image-marker image-marker-date">2022-01-12T09:45:00</span>
-                <p class="article-description">description</p><p class="article-read-more"></p>
-            </div>
-        </article>
-    <!-- /wp:block-article/article-block -->',
+    'content'    => '<!-- wp:group {"layout":{"type":"constrained"}} -->
+                <div class="wp-block-group">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img alt=""/></figure>
+                    <!-- /wp:image -->
+
+                    <!-- wp:heading -->
+                    <h2>Heading</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p>Paragraph</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph -->
+                    <p>Read More</p>
+                    <!-- /wp:paragraph -->
+                </div>
+        <!-- /wp:group -->',
 );
