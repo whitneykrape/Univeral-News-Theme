042dbbe7a6e107d972c5f721f0bd3206e44f8a75
Whitney Krape <whitney@formfunction.io>
2023-03-30 22:23:05 -0400

    WordPress update.

---
+++ base-wordpress/wordpress/wp-content/themes/twentytwentytwo-slimmed/inc/patterns/block-article.php
@@ -0,0 +1,16 @@
+
+<?php
+return array(
+    'title'      => __( 'Block Article', 'slim-2022' ),
+    'categories' => array( 'slim' ),
+    'content'    => '<!-- wp:block-article/article-block {"mediaID":21,"imagelink":"http://ff-pitin.local/_becauseyouhaveto"} -->
+    <article class="wp-block-block-article-article-block">
+        <a class="wp-block-image article-image imagelink" href="http://ff-pitin.local/_becauseyouhaveto" target="_blank" rel="noopener noreferrer"><img src="http://FF-PITIN.local/_becauseyouhaveto/sparewordpress/wp-content/uploads/2022/01/placeholder.jpg" alt=""/></a>
+        <div class="article-text-block">
+            <h3 class="article-title">test</h3>
+            <span class="image-marker image-marker-date">2022-01-12T09:45:00</span>
+            <p class="article-description">description</p><p class="article-read-more"></p>
+        </div>
+    </article>
+    <!-- /wp:block-article/article-block -->',
+);
