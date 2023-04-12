5037597e5bf4a9098be09e790bf6b3cf2a2dc74b
Whitney Krape <whitney@formfunction.io>
2023-04-11 22:33:57 -0400

    Removing unneeded JS blocks.

--- dist/html/blocks/block-article.js
+++
@@ -1,9 +0,0 @@
-/* Should just be article space, no wrapper. Not sure what the section content is for in Ghost. */
-
-<?php
-/* wordpress-start */
-/* Change to a block made in WordPress that can be loaded as a pattern. */
-
-/* Not a block, a pattern. */
-
-/* wordpress-end */
