35510af326f81cd37a3c60fc2e34da77b50e2425
Whitney Krape <whitney@formfunction.io>
2023-06-07 21:27:02 -0400

    Removing moved pieces.

--- src/dev/wordpress-block-article/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0d2b205b7bd05335b6065409d6aa85de');
